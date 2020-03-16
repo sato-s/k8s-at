e11fc893e3c75b68b403c51f374862008658a85a
sato.sawahiko <sato.sawahiko@jp.fujitsu.com>
2020-03-16 00:58:47 +0000

    Implemented Reconci loop

--- api/v1/at_types.go
+++ api/v1/at_types.go
@@ -17,7 +17,16 @@ limitations under the License.
 package v1
 
 import (
+	"fmt"
+	kbatch "k8s.io/api/batch/v1"
+	batchv1beta1 "k8s.io/api/batch/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"time"
+)
+
+const (
+	ScheduledTimeAnnotation = "my.domain/schduledTime"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -25,28 +34,101 @@ import (
 
 // ATSpec defines the desired state of AT
 type ATSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of AT. Edit AT_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// JobTemplate that is run by AT
+	JobTemplate batchv1beta1.JobTemplateSpec `json:"jobTemplate"`
+	// Time which we run this job
+	Schedule *metav1.Time `json:"schedule"`
 }
+
+type ATScheduleStatus string
+
+const (
+	Succeeded ATScheduleStatus = "Succeeded"
+	Stale     ATScheduleStatus = "Stale"  // TODO
+	Failed    ATScheduleStatus = "Failed" // TODO
+	Running   ATScheduleStatus = "Running"
+	Pending   ATScheduleStatus = "Pending" // TODO
+)
 
 // ATStatus defines the observed state of AT
 type ATStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Status of AT
+	// +optional
+	LastScheduleTime *metav1.Time `json:"lastScheduleTime,omitempty"`
+	// AT Status
+	// +optional
+	ATScheduleStatus ATScheduleStatus `json:"status,omitempty"`
+}
+
+// Update attribute considering childJob status
+func (ats *ATStatus) UpdateFromChildJob(childJob kbatch.Job) error {
+	// Set LastScheduleTime
+	timeRaw := childJob.Annotations[ScheduledTimeAnnotation]
+	if len(timeRaw) == 0 {
+		return fmt.Errorf("Failed to get %s from childJob", ScheduledTimeAnnotation)
+	}
+
+	timeParsed, err := time.Parse(time.RFC3339, timeRaw)
+	if err != nil {
+		return fmt.Errorf("Failed to parse time annotation %v", err)
+	}
+	t := metav1.NewTime(timeParsed)
+	ats.LastScheduleTime = &t
+
+	if (childJob.Spec.Completions == nil && childJob.Status.Succeeded == 1) ||
+		(*childJob.Spec.Completions == childJob.Status.Succeeded) {
+		// we consider child job is succeeded if this condition is met
+		ats.ATScheduleStatus = Succeeded
+	} else {
+		// TODO we need to handle Failed,Pending
+		ats.ATScheduleStatus = Running
+	}
+	return nil
 }
 
 // +kubebuilder:object:root=true
-
-// AT is the Schema for the ats API
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="status",type=string,JSONPath=`.status.status`
+// +kubebuilder:printcolumn:name="lastScheduleTime",type=string,JSONPath=`.status.lastScheduleTime`
+// +kubebuilder:printcolumn:name="schdule",type=string,JSONPath=`.spec.schedule`
 type AT struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	Spec   ATSpec   `json:"spec,omitempty"`
 	Status ATStatus `json:"status,omitempty"`
+}
+
+func (at *AT) ChildJobName() string {
+	return fmt.Sprintf("%s-%d", at.Name, at.Spec.Schedule.Unix())
+}
+
+func (at *AT) ChildJobNameSpacedName() types.NamespacedName {
+	return types.NamespacedName{
+		Namespace: at.Namespace,
+		Name:      at.ChildJobName(),
+	}
+}
+
+func (at *AT) ConstructChildJob(scheduleTime time.Time) *kbatch.Job {
+	labels := make(map[string]string)
+	annotations := make(map[string]string)
+	for k, v := range at.Spec.JobTemplate.Annotations {
+		annotations[k] = v
+	}
+	annotations[ScheduledTimeAnnotation] = scheduleTime.Format(time.RFC3339)
+	for k, v := range at.Spec.JobTemplate.Labels {
+		labels[k] = v
+	}
+	return &kbatch.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels:      labels,
+			Annotations: annotations,
+			Name:        at.ChildJobName(),
+			Namespace:   at.Namespace,
+		},
+		Spec: *at.Spec.JobTemplate.Spec.DeepCopy(),
+	}
 }
 
 // +kubebuilder:object:root=true
--- controllers/at_controller.go
+++ controllers/at_controller.go
@@ -19,10 +19,15 @@ package controllers
 import (
 	"context"
 
+	"fmt"
 	"github.com/go-logr/logr"
+	kbatch "k8s.io/api/batch/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"time"
 
 	batchv1 "github.com/sato-s/k8s-at/api/v1"
 )
@@ -36,18 +41,96 @@ type ATReconciler struct {
 
 // +kubebuilder:rbac:groups=batch.my.domain,resources=ats,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=batch.my.domain,resources=ats/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get
 
 func (r *ATReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("at", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("at", req.NamespacedName)
+	now := time.Now()
 
-	// your logic here
+	// Get AT. exit if we can't find this.
+	var at batchv1.AT
+	if err := r.Get(ctx, req.NamespacedName, &at); err != nil {
+		log.Error(err, "unable to fetch AT")
+		// we'll ignore not-found errors, since they can't be fixed by an immediate
+		// requeue (we'll need to wait for a new notification), and we can get them
+		// on deleted requests.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	return ctrl.Result{}, nil
+	var childJob kbatch.Job
+	// Try to find child job
+	err := r.Get(ctx, at.ChildJobNameSpacedName(), &childJob)
+	if err == nil {
+		// We found child job
+		log.Info("child job found", "Job", childJob.Name)
+		if err := at.Status.UpdateFromChildJob(childJob); err == nil {
+			if err := r.Status().Update(ctx, &at); err != nil {
+				return ctrl.Result{}, fmt.Errorf("Failed to update at status %v", err)
+			} else {
+				log.Info("Succesfully update AT info")
+				return ctrl.Result{}, nil
+			}
+		} else {
+			return ctrl.Result{}, fmt.Errorf("Failed to update at status %v", err)
+		}
+	} else if !apierrors.IsNotFound(err) {
+		// We have api error other than NotFound
+		log.Error(err, "Failed to fetch child job")
+		return ctrl.Result{}, err
+	}
+	// We dont' have child job
+	nextRunTime := at.Spec.Schedule.Sub(now)
+	if nextRunTime < 0 {
+		log.Info("Time to run. create child job.")
+		newJob := at.ConstructChildJob(now)
+		// Set owner reference to newJob
+		if err := ctrl.SetControllerReference(&at, newJob, r.Scheme); err != nil {
+			return ctrl.Result{}, fmt.Errorf("Failed to set owener reference to newJob %v", err)
+		}
+		if err := r.Create(ctx, newJob); err != nil {
+			return ctrl.Result{}, fmt.Errorf("Failed to create newJob %v", err)
+		} else {
+			log.Info("Successfully create newJob", "job", newJob.Name)
+			return ctrl.Result{}, nil
+		}
+	} else {
+		log.Info("It's not time to run child job", "now", now, "next run", nextRunTime)
+		at.Status.ATScheduleStatus = batchv1.Pending
+		if err := r.Status().Update(ctx, &at); err != nil {
+			return ctrl.Result{}, fmt.Errorf("Failed to update at status %v", err)
+		} else {
+			return ctrl.Result{RequeueAfter: nextRunTime}, nil
+		}
+	}
+
 }
 
+var (
+	jobOwnerKey = ".metadata.controller"
+	apiGVStr    = batchv1.GroupVersion.String()
+)
+
 func (r *ATReconciler) SetupWithManager(mgr ctrl.Manager) error {
+
+	if err := mgr.GetFieldIndexer().IndexField(&kbatch.Job{}, jobOwnerKey, func(rawObj runtime.Object) []string {
+		// grab the job object, extract the owner...
+		job := rawObj.(*kbatch.Job)
+		owner := metav1.GetControllerOf(job)
+		if owner == nil {
+			return nil
+		}
+		if owner.APIVersion != apiGVStr || owner.Kind != "AT" {
+			return nil
+		}
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&batchv1.AT{}).
+		Owns(&kbatch.Job{}).
 		Complete(r)
 }
